6a124b70ebfb17cc37a6ac5f572c5957dbb6b3be
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-03-02 02:46:15 -0300

    Usando Feign

--- projetofeign2/src/main/java/com/dio/projetofeign2/Projetofeign2Application.java
+++ projetofeign2/src/main/java/com/dio/projetofeign2/Projetofeign2Application.java
@@ -2,8 +2,10 @@ package com.dio.projetofeign2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Projetofeign2Application {
 
 	public static void main(String[] args) {
